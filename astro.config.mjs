65c61016735ecdda5481faf19d2ab59aba745f3f
Jiahong Long <jiahong888long@gmail.com>
2025-12-30 00:02:08 -0500

    sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://j1ah0ng.github.io',
   base: '/plog',
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+    ],
     shikiConfig: {
       theme: 'github-light',
       wrap: true,
